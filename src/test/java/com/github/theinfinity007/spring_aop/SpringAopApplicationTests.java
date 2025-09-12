a727b03796f4a9bef4addc3880c92a00faa603cd
Ram Chandra <46575254+TheInfinity007@users.noreply.github.com>
2025-09-12 22:53:03 +0530

    setup

---
+++ src/test/java/com/github/theinfinity007/spring_aop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.theinfinity007.spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
